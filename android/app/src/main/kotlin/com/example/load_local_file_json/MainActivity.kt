faf12379205ca0cccd8871274468a5ae849f1c3e
Jean Zonta <jeanzonta777@hotmail.com.br>
2022-12-25 20:45:52 -0300

    abrindo arquivo json e listando os dados

---
+++ android/app/src/main/kotlin/com/example/load_local_file_json/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.load_local_file_json
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
+//https://www.kindacode.com/article/how-to-read-local-json-files-in-flutter/
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/json/sample.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Kindacode.com',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: readJson,
+              child: const Text('Load Data'),
+            ),
+
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+                    child: ListView.builder(
+                      itemCount: _items.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          key: ValueKey(_items[index]["id"]),
+                          margin: const EdgeInsets.all(10),
+                          color: Colors.amber.shade100,
+                          child: ListTile(
+                            leading: Text(_items[index]["id"]),
+                            title: Text(_items[index]["name"]),
+                            subtitle: Text(_items[index]["description"]),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
